b91081977dd592bed1ae5f5b5a368581a9cc1990
Asuman Aydın <asuman.aydin6709@gmail.com>
2020-05-17 22:29:59 +0300

    Add files via upload

---
+++ src/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to the MaxBet</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+      <header>
+          <div class="wrapper">
+            <div class="logo">
+              <img src="image/" alt="">
+          </div>
+          <u1 class="nav-area">
+          </ui>
+        </div>
+        <div class="welcome-text">
+          <h1> Maximum Gaining For Maximum Betters!</h1>
+          <a href="#"> Begin Betting! </a>
+          <a href="#"> Begin Editoring! </a>
+          <a href="#"> You wanna be Admin! </a>
+          <a href="#"> Already have account Better? </a>
+          <a href="#"> Already have account Editor? </a>
+          <a href="#"> Already have account Admin? </a>
+      </header>
+  </body>
+</html>
---
+++ src/user_login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to the MaxBet</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  </head>
+  <body>
+      <header>
+        <div class="welcome-text">
+          <h1> welcome back user!</h1>
+          <div class="container">
+            <div class="login-box">
+            <div class="row">
+              <div class="col-md-6 login-mid">
+                <form action="validation.php" method="post">
+                  <div class="form-group">
+                  <label> Username </label>
+                 <input type="text" name="username" class="form-control" required>
+               </div>
+               <div class="form-group">
+               <label> Password </label>
+              <input type="password" name="password" class="form-control" required>
+              </div>
+              <button type="submit" class="btn btn-primary"> Login </button>
+              <br>
+              </<br>
+              <a href="#" class="to_register">Don't have account yet? Join us</a>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </header>
+  </body>
+</html>
