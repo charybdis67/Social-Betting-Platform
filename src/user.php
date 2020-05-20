16d2b7cfcdea04800257443830e710d8c0dc24a4
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2020-05-20 13:00:27 +0300

    Add files via upload

---
+++ src/user.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>CSS User Profile Card</title>
+    <link rel="stylesheet" href="css/user.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="left">
+        <img src="image/User-profile-webpage-account-interface-512.png"
+             alt="user" width="200">
+        <h4>@username</h4>
+        <p>UI Developer</p>
+        <div class="social_media">
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="right">
+        <div class="info">
+            <h3>Information</h3>
+            <div class="info_data">
+                <div class="data">
+                    <h4>Email</h4>
+                    <p>alex@gmail.com</p>
+                </div>
+                <div class="data">
+                    <h4>Phone</h4>
+                    <p>0001-213-998761</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="betslips">
+            <h3>BETSLIPS</h3>
+            <div class="projects_data">
+                <div class="data">
+                    <h4>Recent</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="data">
+                    <h4>Most Viewed</h4>
+                    <p>dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="communities">
+            <h3>COMMUNITIES</h3>
+            <div class="projects_data">
+                <div class="data">
+                    <h4>Recent</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="data">
+                    <h4>Most Viewed</h4>
+                    <p>dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+        <div class="communities">
+            <h3>TOTAL BALANCE: </h3>
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
