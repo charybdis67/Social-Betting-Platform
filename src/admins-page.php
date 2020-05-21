25a5e96bc232ebfa453aa8d654398bcd627995c3
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2020-05-22 00:18:40 +0300

    Create admins-page.php

---
+++ src/admins-page.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Profile</title>
+    <link rel="stylesheet" href="css/admin-profile.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	.checked {
+	  color: orange;
+	}
+	</style>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="left">
+        <img src="image/User-profile-webpage-account-interface-512.png"
+             alt="user" width="200">
+		<h3>ADMIN'S PAGE</h3>	 
+        <h4>@adminusername</h4>
+        <p>I cam the moderator of this betting platform.</p>
+        <div class="social_media">
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="right">
+        <div class="info">
+            <h3>Information</h3>
+            <div class="info_data">
+                <div class="data">
+                    <h4>Email</h4>
+                    <p>admin@gmail.com</p>
+                </div>
+                <div class="data">
+                    <h4>Phone</h4>
+                    <p>0(555)9872345</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="betslips">
+            <h3>POSTS</h3>
+            <div class="data">
+                    <h4>Recent</h4>
+					<h3>
+					Manchester United make £3.3m financial loss in Third Quarter Fiscal 2020 Results
+					</h3>
+                    <p>United record overall loss of £3.3m from January 1 to March 31, primarily down to 51.7 per cent decrease in broadcast revenue
+						<br><br>Last Updated: 21/05/20 3:34pm</p>
+                </div>
+        </div>
+
+        <div class="communities">
+            <h3>BETS SUGGESTED</h3>
+            <div class="projects_data">
+                <div class="data">
+                    <h4>Recent</h4>
+                    <p>BLR<br>
+						19:00
+						1
+						Neman Grodno
+						Torpedo Belaz Zhodino
+						12.50
+						X2.75
+						22.20
+						+0
+						<br>AL2
+						19:30
+						1
+						Nürnberg
+						Erzgebirge Aue
+						11.75
+						X2.95
+						23.15
+						+0
+						</p>
+                </div>
+                <div class="data">
+                    <h4>Most Viewed</h4>
+                    <p>TTK<br>
+					18:10
+					3
+					Vorsulyak, Yaroslav
+					Savenkov, Oleg
+					11.40
+					22.05
+					+0
+					<br>TTK<br>
+					18:20
+					3
+					Laevsky, Maksim
+					Alekseenko, Dmytro
+					12.25
+					21.30
+					+0
+					</p>
+                </div>
+            </div>
+        </div>
+        <div class="communities">
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
