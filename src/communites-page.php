f5d600c7885b169e261ccd2a58c6c18f1f76c194
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2020-05-22 11:42:12 +0300

    Update communites-page.php

--- src/communites-page.php
+++ src/communites-page.php
@@ -4,9 +4,10 @@
 
 </head>
 <body>
-<link rel="stylesheet" href="communities.css">
+<link rel="stylesheet" href="css/communities.css">
     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 <h2>Communities Gallery</h2>
+
 
 <div class="responsive">
   <div class="gallery">
@@ -43,6 +44,181 @@
   </div>
 </div>
 <div class="clearfix"></div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/bjk.jpg">
+      <img src="image/bjk.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.forzabesiktas.com/';">
+    </a>
+    <div class="desc">ÇARŞI</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/fb.jpg">
+      <img src="image/fb.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://m.superfb.com/';">
+    </a>
+    <div class="desc">Genç Fenerbahçeliler</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/gs.jpg">
+	  <img src="image/gs.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.webaslan.com/';">
+    </a>
+    <div class="desc">UltrAslan</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/ts.jpg">
+      <img src="image/ts.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.trabzonspor.org.tr/tr';">
+    </a>
+    <div class="desc">Trabzonsporlular</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/bjk.jpg">
+      <img src="image/bjk.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.forzabesiktas.com/';">
+    </a>
+    <div class="desc">ÇARŞI</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/fb.jpg">
+      <img src="image/fb.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://m.superfb.com/';">
+    </a>
+    <div class="desc">Genç Fenerbahçeliler</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/gs.jpg">
+	  <img src="image/gs.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.webaslan.com/';">
+    </a>
+    <div class="desc">UltrAslan</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/ts.jpg">
+      <img src="image/ts.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.trabzonspor.org.tr/tr';">
+    </a>
+    <div class="desc">Trabzonsporlular</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/bjk.jpg">
+      <img src="image/bjk.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.forzabesiktas.com/';">
+    </a>
+    <div class="desc">ÇARŞI</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/fb.jpg">
+      <img src="image/fb.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://m.superfb.com/';">
+    </a>
+    <div class="desc">Genç Fenerbahçeliler</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/gs.jpg">
+	  <img src="image/gs.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.webaslan.com/';">
+    </a>
+    <div class="desc">UltrAslan</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/ts.jpg">
+      <img src="image/ts.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.trabzonspor.org.tr/tr';">
+    </a>
+    <div class="desc">Trabzonsporlular</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/bjk.jpg">
+      <img src="image/bjk.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.forzabesiktas.com/';">
+    </a>
+    <div class="desc">ÇARŞI</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/fb.jpg">
+      <img src="image/fb.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://m.superfb.com/';">
+    </a>
+    <div class="desc">Genç Fenerbahçeliler</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/gs.jpg">
+	  <img src="image/gs.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.webaslan.com/';">
+    </a>
+    <div class="desc">UltrAslan</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/ts.jpg">
+      <img src="image/ts.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.trabzonspor.org.tr/tr';">
+    </a>
+    <div class="desc">Trabzonsporlular</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/bjk.jpg">
+      <img src="image/bjk.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.forzabesiktas.com/';">
+    </a>
+    <div class="desc">ÇARŞI</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/fb.jpg">
+      <img src="image/fb.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://m.superfb.com/';">
+    </a>
+    <div class="desc">Genç Fenerbahçeliler</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/gs.jpg">
+	  <img src="image/gs.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.webaslan.com/';">
+    </a>
+    <div class="desc">UltrAslan</div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="image/ts.jpg">
+      <img src="image/ts.jpg" alt="Team" width="600" height="400" onclick="window.location.href = 'https://www.trabzonspor.org.tr/tr';">
+    </a>
+    <div class="desc">Trabzonsporlular</div>
+  </div>
+</div>
 
 
 </body>
